7ed075500ca8b61b60e7e524044b58dfaa478dc3
Marbocatcat <mar.bocatcat@gmail.com>
2017-05-28 19:59:03 -0500

    navigation

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import { AppRegistry } from 'react-native'
-import AppNavigator from './app/navigation/AppNavigator'
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import AppNavigator from './app/navigation/AppNavigator';
 
-class WheresMyCoffee extends Component {
-  render() {
-    return (
-          <AppNavigator
-            initialRoute={{ident: "Search"}} />
-    )
-  }
+
+const WheresMyCoffee = () => {
+  return (
+    <AppNavigator initialRoute={{ident: "Search"}}/>
+  );
 }
 
-AppRegistry.registerComponent('WheresMyCoffee', () => WheresMyCoffee)
+AppRegistry.registerComponent('WheresMyCoffee', () => WheresMyCoffee);
